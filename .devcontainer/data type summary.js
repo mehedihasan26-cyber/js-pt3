9b775e658b53e75073700826846556a4bd4dc608
mehedihasan26-cyber <mehedihasan180580@gmail.com>
2025-01-14 12:22:11 +0000

    cng

---
+++ .devcontainer/data type summary.js
@@ -0,0 +1,11 @@
+//premitive data type 
+// 7 types of premitive data type : string, number ,bolean,symbol,null,undefined ,bigint.
+const string="mehedi "
+const num= 123 
+const isLoggedIn= false;
+let  userEmail;
+const id =Symbol("123")
+const id2= Symbol("123")
+console.log(id===id2);
+//non premitive data type 
+// there are : array, object , functions 
